faaf63256fe04d0a687c0ba5b154c5c4a95912f3
Tyler Pastor <tpast001@wch136-24.cs.ucr.edu>
2021-04-17 01:12:45 -0700

    testing part 5

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: tpast001
  *  Partner(s) Name: N/A
  *	Lab Section: 023
- *	Assignment: Lab #4  Exercise #4
+ *	Assignment: Lab #4  Exercise #5
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,10 +12,10 @@
 #include "simAVRHeader.h"
 #endif
 
-int code[] = {0x01, 0x02, 0x01};
+int code[] = {0x04, 0x01, 0x02, 0x01};
 int count = 0;
 
-enum SM1_STATES {SM1_SMStart, SM1_pound, SM1_lock, SM1_unlocked,SM1_Wait1, SM1_Wait2 }SM1_STATE;//SM1_Waitpound, SM1_Y, SM1_Lockpound, SM1_WaitLockpound, SM1_LockY} SM1_STATE;
+enum SM1_STATES {SM1_SMStart, SM1_lock, SM1_unlocked, SM1_reset1, SM1_reset2} SM1_STATE; //SM1_Wait1, SM1_Wait2 } SM1_STATE;
 void Tick_Toggle() { 
 	switch(SM1_STATE) { 
      		case SM1_SMStart:
@@ -23,75 +23,55 @@ void Tick_Toggle() {
          		break;
 
 		case SM1_lock:
-			if(PINA == 0x04){
-				SM1_STATE = SM1_Wait1;
-			}
-			else if(PINA == 0x02 || PINA == 0x01){
-				SM1_STATE = SM1_lock;
-			}
-			else{
-				SM1_STATE = SM1_lock;
-			}      		
-			break;
-/*
-		case SM1_pound:
-			if(PINA == 0x04){
-				SM1_STATE = SM1_pound;	
+			if(PINA == code[count]){
+				if(count == 3){
+					SM1_STATE = SM1_unlocked;
+				}
+				else{
+					count++;
+					SM1_STATE = SM1_lock;
+				}
 			}
 			else if(PINA == 0x00){
-				SM1_STATE = SM1_Waitpound;
+				SM1_STATE = SM1_lock;		
 			}
 			else{
-				SM1_STATE = SM1_lock;
-			}
+				SM1_STATE = SM1_reset1;
+			} 		
 			break;
 
-		case SM1_Y:
-			if(PINA == 0x02){
-				SM1_STATE = SM1_Y;
+		case SM1_unlocked:
+			if(PINA == code[count]){
+				if(count == 3){
+					SM1_STATE = SM1_lock;
+				}
+				else{
+					count++;
+					SM1_STATE = SM1_unlocked;
+				}
 			}
 			else if(PINA == 0x00){
-				SM1_STATE = SM1_unlocked;
-			}
-			else{
-				SM1_STATE = SM1_lock;
-			}
-			break;
-
-		case SM1_X:
-			SM1_STATE = SM1_lock;
-			break;
-		
-		case SM1_Waitpound:
-			if(PINA == 0x02){
-				SM1_STATE = SM1_Y;
-			}
+				SM1_STATE = SM1_unlocked;		
+			}		
 			else if(PINA == 0x80){
 				SM1_STATE = SM1_lock;
 			}
-			else if(PINA == 0x00){
-				SM1_STATE = SM1_Waitpound;
-			}
 			else{
-				SM1_STATE = SM1_lock;	
-			}
-			break;
-*/
-		case SM1_unlocked:
-			if(PINA == 0x80){
-				SM1_STATE = SM1_lock;
-			}
-			else if(PINA == 0x04){
-				SM1_STATE = SM1_Wait2;
-			}		
-			else{
-				SM1_STATE = SM1_unlocked;
-			}
+				SM1_STATE = SM1_reset2;
+			}			
 			break;
 		
-		case SM1_Wait1:
+		case SM1_reset1:
+			SM1_STATE = SM1_lock;			
+			break;
+
+		case SM1_reset2:
+			SM1_STATE = SM1_unlocked;			
+			break;			
+		
+	/*	case SM1_Wait1:
 			if(PINA == code[count]){
-				if(count == 2){
+				if(count == 3){
 					SM1_STATE = SM1_unlocked;
 				}
 				else{
@@ -110,7 +90,7 @@ void Tick_Toggle() {
 		
 		case SM1_Wait2:
 			if(PINA == code[count]){
-				if(count == 2){
+				if(count == 3){
 					SM1_STATE = SM1_lock;
 				}
 				else{
@@ -120,55 +100,14 @@ void Tick_Toggle() {
 			}
 			else if(PINA == 0x00){
 				SM1_STATE = SM1_Wait2;		
-			}
+			}		
 			else if(PINA == 0x80){
 				SM1_STATE = SM1_lock;
 			}
 			else{
 				SM1_STATE = SM1_unlocked;
 			}
-			break;
-/*
-		case SM1_Lockpound:
-			if(PINA == 0x04){
-				SM1_STATE = SM1_Lockpound;	
-			}
-			else if(PINA == 0x00){
-				SM1_STATE = SM1_WaitLockpound;
-			}
-			else{
-				SM1_STATE = SM1_unlocked;
-			}
-			break;
-
-		case SM1_LockY:
-			if(PINA == 0x02){
-				SM1_STATE = SM1_LockY;
-			}
-			else if(PINA == 0x00){
-				SM1_STATE = SM1_lock;
-			}
-			else{
-				SM1_STATE = SM1_unlocked;
-			}
-			break;
-		
-		case SM1_WaitLockpound:
-			if(PINA == 0x02){
-				SM1_STATE = SM1_LockY;
-			}
-			else if(PINA == 0x80){
-				SM1_STATE = SM1_unlocked;
-			}
-			else if(PINA == 0x00){
-				SM1_STATE = SM1_WaitLockpound;
-			}
-			else{
-				SM1_STATE = SM1_unlocked;	
-			}
-			break;*/
-
-		
+			break; */
 
       		default:
          		SM1_STATE = SM1_lock;
@@ -181,36 +120,26 @@ void Tick_Toggle() {
 
 		case SM1_lock:
 			PORTB = 0x00;
+			//count = 0;
+         		break;
+
+		case SM1_reset1:
 			count = 0;
-         		break;
-      /*		
-		case SM1_pound:
-         		break;
+			break;
 
-      		case SM1_Waitpound:
-        		break;
-			
-		case SM1_Y:
-        		break;
+		case SM1_reset2:
+			count = 0;
+			break;
 
-		case SM1_Lockpound:
-         		break;
-
-      		case SM1_WaitLockpound:
-        		break;
-			
-		case SM1_LockY:
-        		break;	*/
-
-		case SM1_Wait1:
+		/*case SM1_Wait1:
         		break;
 
 		case SM1_Wait2:
-        		break;
+        		break;*/
 
 		case SM1_unlocked:
          		PORTB = 0x01;
-			count = 0;
+			//count = 0;
         		break; 	
 	}
 }
